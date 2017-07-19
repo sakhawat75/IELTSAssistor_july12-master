ab164c4e423f4b69d4a5d5387d812333e38f6e83
HussainJuned <hussainjuned99@gmail.com>
2017-07-20 01:44:41 +0600

    commit confirm password

--- app/src/main/java/team_uganda/ieltsassistor/BookStorage.java
+++ app/src/main/java/team_uganda/ieltsassistor/BookStorage.java
@@ -6,12 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class BookStorage extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_storage);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() == null){
+            Toast.makeText(this, "You need to Signin first to\nDownload books", Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        }
     }
     public void readBook1(View v)
     {
